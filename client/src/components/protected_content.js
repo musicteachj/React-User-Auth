2599fffa3c95a56aaca138a6ccd5b4b3cd99c311
musicteachj <jimilittle3@hotmail.com>
2017-10-13 12:56:10 -0400

    init commit
    init commit

---
+++ client/src/components/protected_content.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class ProtectedContent extends Component {
+  componentWillMount() {
+    this.props.fetchMessage();
+  }
+
+  render() {
+    return (
+    	<div>
+    	<h1 className="jumbotron">Hello</h1>
+      <div className="msg">{this.props.message}</div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { message: state.auth.message };
+}
+
+export default connect(mapStateToProps, actions)(ProtectedContent);
